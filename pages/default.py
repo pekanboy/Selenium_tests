c71706dd01c12fef80969e15100d3e7e71512484
pekanboy <egorovaleksey0@mail.ru>
2021-10-24 14:38:05 +0300

    add default page class

---
+++ pages/default.py
@@ -0,0 +1,15 @@
+from urllib.parse import urljoin
+
+
+class DefaultPage:
+    BASE_URL = 'https://findfreelancer.ru'
+    PATH = ''
+
+    def __init__(self, driver):
+        self.driver = driver
+
+    # Открытие страницы класса
+    def open(self):
+        url = urljoin(self.BASE_URL, self.PATH)
+        self.driver.get(url)
+        self.driver.maximize_window()
