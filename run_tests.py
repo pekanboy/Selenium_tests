5fa7a36e92404111c9ac6c293f440b6dfd329aed
Yaroslav <belykh-yar@yandex-team.ru>
2021-11-04 16:34:55 +0300

    Done

--- run_tests.py
+++ run_tests.py
@@ -4,12 +4,18 @@ import unittest
 from tests.AllOrdersStep import AllOrdersTest
 from tests.AuthTest import AuthTest
 from tests.SearchTest import SearchTest
+from tests.MainTest import MainTest
+from tests.NavbarsTest import NavbarsTest
+from tests.SettingsTests import SettingsTests
 
 if __name__ == '__main__':
     suite = unittest.TestSuite((
+        unittest.makeSuite(NavbarsTest),
         unittest.makeSuite(AuthTest),
         unittest.makeSuite(SearchTest),
         unittest.makeSuite(AllOrdersTest),
+        unittest.makeSuite(MainTest),
+        unittest.makeSuite(SettingsTests),
     ))
 
     result = unittest.TextTestRunner().run(suite)
